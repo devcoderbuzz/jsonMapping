99ff078e6d066e21dfafdddd8502d1933b94f232
DevCoder <devcoder@DevCoders-MacBook-Pro.local>
2025-04-23 00:59:24 +0800

    added sample json

--- src/main/java/com/yourorg/jsontransform/model/JsonTransformRequest.java
+++ src/main/java/com/yourorg/jsontransform/model/JsonTransformRequest.java
@@ -1,10 +1,12 @@
 package com.yourorg.jsontransform.model;
 
+import java.util.List;
 import java.util.Map;
 
 public class JsonTransformRequest {
     private Map<String, Object> sourceJson;
     private Map<String, String> mappingJson;
+    private List<Map<String, Object>> sourceJsonList;
 
     public Map<String, Object> getSourceJson() {
         return sourceJson;
@@ -20,5 +22,13 @@ public class JsonTransformRequest {
 
     public void setMappingJson(Map<String, String> mappingJson) {
         this.mappingJson = mappingJson;
+    }
+
+    public List<Map<String, Object>> getSourceJsonList() {
+        return sourceJsonList;
+    }
+
+    public void setSourceJsonList(List<Map<String, Object>> sourceJsonList) {
+        this.sourceJsonList = sourceJsonList;
     }
 }
